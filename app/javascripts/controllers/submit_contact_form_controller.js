ffd7b8662388813b9b820163dbe5cfe67d18ff65
Slavka Tsankova <smcankova@gmail.com>
2014-12-10 20:36:52 +0200

    - remove padding top on the header - fixed bugs about not filled field - add new controller for new library

--- app/javascripts/controllers/submit_contact_form_controller.js
+++ app/javascripts/controllers/submit_contact_form_controller.js
@@ -1,31 +1,57 @@
 angular.module('website').
-    controller('SubmitContactFormController', ['$scope', 'submit_contact_form_factory', function($scope, submit_contact_form_factory) {
-        $scope.contactForm = {};
-        $scope.submitForm = submitForm;
-        $scope.status = {};
+    controller('SubmitContactFormController', ['$scope', 'submit_contact_form_factory', '$location', '$document',
+      function($scope, submit_contact_form_factory, $location, $document) {
+      $scope.contactForm = {};
+      $scope.submitForm = submitForm;
+      $scope.status = {};
+      var duration= 1500;
+      var offset= 30;
+      var someElement;
 
-        function submitForm(odt_file) {
-          $scope.loading = true;
 
-            submit_contact_form_factory.processContactForm($scope.contactForm)
-              .success(function (result) {
-                  $scope.status = {};
-                  if(result['errors']) {
-                    $scope.status.code = result['status'];
-                    $scope.status.message = 'Please inspect your form and submit it again';
-                    $scope.errors = result['errors'];
-                  } else if(result['status'] == 'ok') {
-                    $scope.status.code = result['status'];
-                    $scope.successful = true;
-                    $scope.status.message = 'You request was sent successfully';
-                  }
-                  $scope.converted = result;
-                  $scope.loading = false;
-              })
-              .error(function (error) {
-                $scope.loading = false;
-                $scope.error_message = 'Some error occured';
-//                 $scope.status = 'Unable to load customer data: ' + error.message;
+      function submitForm(odt_file) {
+        $scope.loading = true;
+        submit_contact_form_factory.processContactForm($scope.contactForm)
+          .success(function (result) {
+            var theFirst = null;
+            $scope.status = {};
+            if (result['errors']) {
+              $scope.status.code = result['status'];
+              $scope.status.message = 'Please inspect your form and submit it again';
+              $scope.errors = result['errors'];
+              angular.forEach(result['errors'], function(value, key) {
+                if(!theFirst) {
+                  theFirst = key;
+                }
               });
+              someElement = angular.element(document.getElementById(theFirst + '_label'));
+              $document.scrollToElement(someElement, offset, duration);
+            } else if (result['status'] == 'ok') {
+              $scope.status.code = result['status'];
+              $scope.successful = true;
+              $scope.status.message = 'You request was sent successfully';
+            }
+            $scope.converted = result;
+            $scope.loading = false;
+          })
+          .error(function (error) {
+            $scope.loading = false;
+            $scope.error_message = 'Some error occured';
+            someElement = angular.element(document.getElementById(theFirst + '_label'));
+            $document.scrollToElement(someElement, offset, duration);
+
+          });
+      };
+
+      $scope.gotoAnchor = gotoAnchor;
+
+      function gotoAnchor(x) {
+        var newHash = x;
+        if ($location.hash() !== newHash) {
+          $location.hash(x);
+        } else {
+          $anchorScroll();
+        }
       }
-}]);
+    }]);
+
