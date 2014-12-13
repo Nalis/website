cdb01b2ad586e882f115e11109ce7b1b101146dc
Valentin Aitken <bostko@gmail.com>
2014-12-13 14:55:08 +0200

    bugfix the seo.php (a little hard to write without highlighting)

--- app/seo.php
+++ app/seo.php
@@ -1,13 +1,16 @@
 <?php
 if(isset($_GET['_escaped_fragment_'])) {
-  $pages = array("about", "contact_form", "contacts", "technologies");
+  $pages = array("/about", "/contact_form", "/contacts", "/technologies");
   $routes = array();
   foreach($pages as $page) {
     $routes[$page] = "./views$page.html";
   }
+  $routes["/"] = $routes["/contact_form"];
+
+  $page = $_GET['_escaped_fragment_'];
 
   if(isset($routes[$page])) {
     include $routes[$page];
+    exit (0);
   }
-  exit (0);
 }
